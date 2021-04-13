7e651bcc168cf274ffdb26e13d88e8e2297c2e17
ADMIN <naqeeb249@gmail.com>
2021-04-13 13:00:31 +0530

    experiment with tagName and className

--- Item Lister/main.js
+++ Item Lister/main.js
@@ -1,4 +1,4 @@
-console.log(document.domain);
+/* console.log(document.domain);
 console.log(document.URL);
 console.log(document.title);
 console.log(document.doctype);
@@ -16,7 +16,7 @@ let headerTitle = document.getElementById('header-Title');
 /* headerTitle.textContent = 'Hello';
 headerTitle.textContent = 'Hi'; */
 
-let header = document.getElementById('main-header');
+/*let header = document.getElementById('main-header');
 
 header.style.borderBottom = 'solid 3px #000';
 
@@ -24,3 +24,20 @@ let title = document.querySelector('.title');
 
 title.style.fontWeight = 'bold';
 title.style.color = 'green';
+
+
+const items = document.getElementsByClassName('list-group-item');
+
+items[2].style.backgroundColor = 'green';
+
+for(let i=0; i<items.length; i++){
+    items[i].style.fontWeight = 'bold';
+ } */
+
+ let li = document.getElementsByTagName('li');
+
+ li[2].style.fontWeight= 'bold';
+
+ for(let i=0; i<li.length; i++){
+     li[i].style.backgroundColor = 'yellow';
+ }
